01b3908294e133c2ea891422df96c8fefdbb27af
zhangyh <zhangyh@innosilicon.com.cn>
2018-08-23 16:07:17 +0800

    add pool record

--- sgminer.c
+++ sgminer.c
@@ -8469,6 +8469,32 @@ struct device_drv *copy_drv(struct device_drv *drv)
 #define DRIVER_FILL_DEVICE_DRV(X) fill_device_drv(&X##_drv);
 #define DRIVER_DRV_DETECT_ALL(X) X##_drv.drv_detect(false);
 
+static int get_nand_access()
+{
+	FILE *fp = NULL;
+	char temp_info[512] = {0};
+	int i = 0;
+
+	fp = fopen("/tmp/pool_info.au","w");
+	if(fp == NULL)
+	{
+		applog(LOG_ERR,"Sorry ,create file failed\n");
+		return -1;
+	}
+	//applog(LOG_ERR,"total users:%d\n",total_users);
+	for(i=0; i<total_users; i++)
+	{
+		sprintf(temp_info, "R:%d %s %s",i, pools[i]->rpc_url, pools[i]->rpc_user);
+		applog(LOG_DEBUG,"pool info:%s\n",temp_info);
+		fwrite(temp_info,1,strlen(temp_info)+1,fp);
+	}
+
+	fclose(fp);
+
+	return 1;
+}
+
+
 int main(int argc, char *argv[])
 {
     struct sigaction handler;
@@ -8659,6 +8685,11 @@ int main(int argc, char *argv[])
 
     /* Use the DRIVER_PARSE_COMMANDS macro to detect all devices */
     DRIVER_PARSE_COMMANDS(DRIVER_DRV_DETECT_ALL)
+
+	if (get_nand_access() != -1)
+		mcompat_record_params();
+	else
+		applog(LOG_ERR,"Failed to get nand access.");
 
     if (opt_display_devs) {
         applog(LOG_ERR, "Devices detected:");
